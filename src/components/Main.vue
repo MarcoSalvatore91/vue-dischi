fbe5f945b2f89b7aae01572e4ce69ff9ed147b5b
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-10 17:08:43 +0100

    added card in main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,12 @@
 <template>
   <section>
-      <div v-for="song in songs" :key="song.response">
-          <img :src="song.poster" :alt="song.author">
+      <div class="container row">
+        <div class="card-song text-center" v-for="song in songs" :key="song.response">
+            <img :src="song.poster" :alt="song.author">
+            <h4 class="white">{{ song.title }}</h4>
+            <div class="white">{{ song.author }}</div>
+            <div class="white">{{ song.year }}</div>
+        </div>
       </div>
   </section>
 </template>
@@ -34,8 +39,20 @@ section {
     height: calc(100vh - 96px);
     background-color: rgb(30,45,59);
 
-    img {
-        height: 120px;
+    .card-song {
+        width: 150px;
+        padding: 10px 10px 20px 10px;
+        background-color: $bgc_grey;
+        margin: 10px 20px;
+
+        img {
+            width: 120px;
+        }
+
+        h4 {
+            margin-top: 10px;
+        }
     }
+
 }
 </style>

2a7e4a9b26b4d0d1ff7ff6b206b85a54b0433f28
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-10 15:28:23 +0100

    npm first build

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <img src="../assets/img/unnamed.png" alt="Logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
